0126f1a198ad605e4b679801cadaef2121db5374
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2024-12-11 20:24:23 -0300

    Initial commit

---
+++ DSRemapperReports.cs
@@ -0,0 +1,205 @@
+﻿using DSRemapper.DSRMath;
+using DSRemapper.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSRemapper.DualShock
+{
+    public class DualShockInputReport : IDSRInputReport
+    {
+        private float bat = 0;
+        public IDS4InReport Raw { get; set; }
+        public float Battery { get => bat; set => bat = Math.Clamp(value, 0f, 1f); }
+        public bool Charging { get; set; }
+        public float[] Axes { get; set; } = [0,0,0,0,0,0];
+
+        public float[] Sliders { get => []; set => _ = value; }
+
+        public bool[] Buttons { get; set; } = [false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+        public DSRPov[] Povs { get; set; } = [new()];
+
+        public DSRVector3[] SixAxes { get; set; } = [new(), new(), new(), new()];
+
+        public DSRQuaternion[] Quaternions { get; set; } = [new(), new()];
+
+        public DSRTouch[] Touch { get; set; } = [new(), new()];
+        public DSRVector2 TouchPadSize { get => new(1920, 943); set => _ = value; }
+        public float DeltaTime { get; set; }
+        public DSRVector3 RawAccel { get { return SixAxes[0]; } set { SixAxes[0] = value; } }
+        public DSRVector3 Gyro { get { return SixAxes[1]; } set { SixAxes[1] = value; } }
+        public DSRVector3 Grav { get { return SixAxes[2]; } set { SixAxes[2] = value; } }
+        public DSRVector3 Accel { get { return SixAxes[3]; } set { SixAxes[3] = value; } }
+        public DSRQuaternion DeltaRotation { get { return Quaternions[0]; } set { Quaternions[0] = value; } }
+        public DSRQuaternion Rotation { get { return Quaternions[1]; } set { Quaternions[1] = value; } }
+        public DualShockInputReport()
+        {
+        }
+        public DualShockInputReport(IDS4InReport raw)
+        {
+            Set(raw);
+        }
+        public void Set(IDS4InReport raw)
+        {
+            Raw = raw;
+            Update();
+        }
+        public void Update()
+        {
+            Axes[0] = Raw.Basic.LX.ToFloat();
+            Axes[1] = Raw.Basic.LY.ToFloat();
+            Axes[2] = Raw.Basic.RX.ToFloat();
+            Axes[3] = Raw.Basic.RY.ToFloat();
+            Axes[4] = Raw.Basic.LTrigger.ToFloat();
+            Axes[5] = Raw.Basic.RTrigger.ToFloat();
+
+            Povs[0].SetDSPov(Raw.Basic.DPad);
+
+            Buttons[0] = Raw.Basic.Square;
+            Buttons[1] = Raw.Basic.Cross;
+            Buttons[2] = Raw.Basic.Circle;
+            Buttons[3] = Raw.Basic.Triangle;
+
+            Buttons[4] = Raw.Basic.L1;
+            Buttons[5] = Raw.Basic.R1;
+            Buttons[6] = Raw.Basic.L2;
+            Buttons[7] = Raw.Basic.R2;
+            Buttons[8] = Raw.Basic.Share;
+            Buttons[9] = Raw.Basic.Options;
+            Buttons[10] = Raw.Basic.L3;
+            Buttons[11] = Raw.Basic.R3;
+
+            Buttons[12] = Raw.Basic.PS;
+            Buttons[13] = Raw.Basic.TPad;
+
+            Battery = Raw.Extended.Battery / 10f;
+            Charging = Raw.Extended.USB;
+
+            SixAxes[1].X = -Raw.Extended.AngularVelocityX * (2000f / 32767f);
+            SixAxes[1].Y = -Raw.Extended.AngularVelocityY * (2000f / 32767f);
+            SixAxes[1].Z = Raw.Extended.AngularVelocityZ * (2000f / 32767f);
+            SixAxes[0].X = -Raw.Extended.AccelerometerX / 8192f;
+            SixAxes[0].Y = -Raw.Extended.AccelerometerY / 8192f;
+            SixAxes[0].Z = Raw.Extended.AccelerometerZ / 8192f;
+
+            Touch[0].Pressed = Raw.Touch.Fingers[0].FingerTouch;
+            Touch[0].Id = Raw.Touch.Fingers[0].FingerId;
+            Touch[0].Pos.X = Raw.Touch.Fingers[0].FingerX;
+            Touch[0].Pos.Y = Raw.Touch.Fingers[0].FingerY;
+            Touch[0].Pos /= TouchPadSize;
+
+            Touch[1].Pressed = Raw.Touch.Fingers[1].FingerTouch;
+            Touch[1].Id = Raw.Touch.Fingers[1].FingerId;
+            Touch[1].Pos.X = Raw.Touch.Fingers[1].FingerX;
+            Touch[1].Pos.Y = Raw.Touch.Fingers[1].FingerY;
+            Touch[1].Pos /= TouchPadSize;
+        }
+        public void UpdateRaw()
+        {
+            BasicInState basic = Raw.Basic;
+            ExtendedInState extended = Raw.Extended;
+
+            TouchStatus touch = Raw.Touch;
+
+            basic.LX = Axes[0].ToSByte();
+            basic.LY = Axes[1].ToSByte();
+            basic.RX = Axes[2].ToSByte();
+            basic.RY = Axes[3].ToSByte();
+            basic.LTrigger = Axes[4].ToByte();
+            basic.RTrigger = Axes[5].ToByte();
+
+            basic.Square = Buttons[0];
+            basic.Cross = Buttons[1];
+            basic.Circle = Buttons[2];
+            basic.Triangle = Buttons[3];
+
+            basic.L1 = Buttons[4];
+            basic.R1 = Buttons[5];
+            basic.L2 = Buttons[6];
+            basic.R2 = Buttons[7];
+            basic.Share = Buttons[8];
+            basic.Options = Buttons[9];
+            basic.L3 = Buttons[10];
+            basic.R3 = Buttons[11];
+
+            basic.PS = Buttons[12];
+            basic.TPad = Buttons[13];
+
+            extended.Battery = (byte)(Battery * 10);
+            extended.USB = Charging;
+
+            extended.AngularVelocityX = (short)(-SixAxes[1].X * (32767f / 2000f));
+            extended.AngularVelocityY = (short)(-SixAxes[1].Y * (32767f / 2000f));
+            extended.AngularVelocityZ = (short)(SixAxes[1].Z * (32767f / 2000f));
+            extended.AccelerometerX = (short)(-SixAxes[0].X * 8192f);
+            extended.AccelerometerY = (short)(-SixAxes[0].Y * 8192f);
+            extended.AccelerometerZ = (short)(SixAxes[0].Z * 8192f);
+
+            Raw.Touch.Fingers[0].FingerTouch = Touch[0].Pressed;
+            Raw.Touch.Fingers[0].FingerId = (byte)Touch[0].Id;
+            Raw.Touch.Fingers[0].FingerX = (short)(Touch[0].Pos.X * TouchPadSize.X);
+            Raw.Touch.Fingers[0].FingerY = (short)(Touch[0].Pos.Y * TouchPadSize.Y);
+
+            Raw.Touch.Fingers[1].FingerTouch = Touch[1].Pressed;
+            Raw.Touch.Fingers[1].FingerId = (byte)Touch[1].Id;
+            Raw.Touch.Fingers[1].FingerX = (short)(Touch[1].Pos.X * TouchPadSize.X);
+            Raw.Touch.Fingers[1].FingerY = (short)(Touch[1].Pos.Y * TouchPadSize.Y);
+
+            Raw.Basic = basic;
+            Raw.Extended = extended;
+        }
+    }
+
+    public class DualShockOutputReport : IDSROutputReport
+    {
+        public IDS4OutReport Raw { get; set; }
+        public float[] Rumble { get; set; } = [0,0];
+        public DSRLight Led { get; set; } = new();
+        public float[] ExtLeds { get => []; set => _ = value; }
+        public DualShockOutputReport()
+        {
+            Led.SetRGB(0, 0, 1, 0.125f);
+        }
+        public DualShockOutputReport(IDS4OutReport raw)
+        {
+            Set(raw);
+        }
+        public void Set(IDS4OutReport raw)
+        {
+            Raw = raw;
+            Update();
+        }
+        public void Update()
+        {
+            Rumble[0] = Raw.State.RumbleRight.ToFloat();
+            Rumble[1] = Raw.State.RumbleLeft.ToFloat();
+            Led.SetRGB(Raw.State.Red.ToFloat(), Raw.State.Green.ToFloat(), Raw.State.Blue.ToFloat());
+            Led.OnTime = Raw.State.FlashOn.ToFloat();
+            Led.OffTime = Raw.State.FlashOff.ToFloat();
+        }
+        public void UpdateRaw()
+        {
+            SetStateData stateData = Raw.State;
+            stateData.RumbleRight = Rumble[0].ToByte();
+            stateData.RumbleLeft = Rumble[1].ToByte();
+            stateData.Red = Led.Red.ToByte();
+            stateData.Green = Led.Green.ToByte();
+            stateData.Blue = Led.Blue.ToByte();
+            stateData.FlashOn = Led.OnTime.ToByte();
+            stateData.FlashOff = Led.OffTime.ToByte();
+            Raw.State = stateData;
+        }
+    }
+    public static class StructExtensions
+    {
+        public static float ToFloat(this byte axis) => axis / 255f;
+        public static float ToFloat(this sbyte axis) => axis / (axis < 0 ? 128f : 127f);
+        public static byte ToByte(this float axis) => (byte)(axis * 255);
+        public static sbyte ToSByte(this float axis) => (sbyte)(axis * (axis < 0 ? 128f : 127f));
+        public static sbyte AxisConvertion(this byte b) => (sbyte)(b ^ 0x80);
+        public static byte AxisConvertion(this sbyte b) => (byte)AxisConvertion((byte)b);
+    }
+}
---
+++ DualShockStructs.cs
@@ -0,0 +1,325 @@
+﻿using DSRemapper.Core.Types;
+using System.Collections.Specialized;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace DSRemapper.DualShock
+{
+    public interface IDS4InReport
+    {
+        public byte ReportId { get; set; }
+        public BasicInState Basic { get; set; }
+        public ExtendedInState Extended { get; set; }
+        public TouchStatus Touch { get; set; }
+        public TouchStatus[] Touches { get; set; }
+        public byte[] CRC { get; set; }
+        public byte[] ToArray();
+    }
+    public interface IDS4OutReport
+    {
+        public SetStateData State { get; set; }
+        public byte[] CRC { get; set; }
+        public byte[] ToArray();
+    }
+
+    #region Input
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 64)]
+    public struct USBStatus : IDS4InReport
+    {
+        private byte reportId = 0;
+        private BasicInState basicState = new();
+        private ExtendedInState extendedState = new();
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        private TouchStatus[] touchStatus = new TouchStatus[3];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        private byte[] padding = new byte[3];
+
+        public byte ReportId { get => reportId; set => reportId = value; }
+        public BasicInState Basic { get => basicState; set => basicState = value; }
+        public ExtendedInState Extended { get => extendedState; set => extendedState = value; }
+        public TouchStatus Touch { get => touchStatus[0]; set => touchStatus[0] = value; }
+        public TouchStatus[] Touches { get => touchStatus; set => touchStatus = value; }
+        public byte[] CRC { get => []; set => _ = value; }
+        public USBStatus() { }
+        public byte[] ToArray()
+        {
+            byte[] raw = new byte[64];
+            GCHandle ptr = GCHandle.Alloc(raw, GCHandleType.Pinned);
+            Marshal.StructureToPtr(this, ptr.AddrOfPinnedObject(), false);
+            ptr.Free();
+            return raw;
+        }
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 78)]
+    public struct BTStatus : IDS4InReport
+    {
+        private byte reportId = 0;
+        private BitVector<byte> misc1 = new();
+        private BitVector<byte> misc2 = new();
+        private BasicInState basicState = new();
+        private ExtendedInState extendedState = new();
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        private TouchStatus[] touchStatus = new TouchStatus[4];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        private byte[] padding = new byte[2];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        private byte[] crc = new byte[4];
+
+        public byte ReportId { get => reportId; set => reportId = value; }
+        public byte PollingRate { get => misc1[0x3F, 0]; set => misc1[0x3F, 0] = value; }
+        public bool EnableCRC { get => misc1[0x40]; set => misc1[0x40] = value; }
+        public bool EnableHID { get => misc1[0x80]; set => misc1[0x80] = value; }
+        public byte EnableMic { get => misc2[0x07, 0]; set => misc2[0x07, 0] = value; }
+        public byte Unk { get => misc2[0x0F, 3]; set => misc2[0x0F, 3] = value; }
+        public bool EnableAudio { get => misc2[0x80]; set => misc2[0x80] = value; }
+        public BasicInState Basic { get => basicState; set => basicState = value; }
+        public ExtendedInState Extended { get => extendedState; set => extendedState = value; }
+        public TouchStatus Touch { get => touchStatus[0]; set => touchStatus[0] = value; }
+        public TouchStatus[] Touches { get => touchStatus; set => touchStatus = value; }
+        public byte[] CRC { get => crc; set => crc = value; }
+
+        public BTStatus() { }
+        public byte[] ToArray()
+        {
+            byte[] raw = new byte[78];
+            GCHandle ptr = GCHandle.Alloc(raw, GCHandleType.Pinned);
+            Marshal.StructureToPtr(this, ptr.AddrOfPinnedObject(), false);
+            ptr.Free();
+            return raw;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 9)]
+    public struct BasicInState
+    {
+        private byte lx = 0;
+        private byte ly = 0;
+        private byte rx = 0;
+        private byte ry = 0;
+        private BitVector<ushort> buttons = new();
+        private BitVector<byte> misc = new();
+        private byte lTrigger = 0;
+        private byte rTrigger = 0;
+
+        public sbyte LX { get => lx.AxisConvertion(); set => lx = value.AxisConvertion(); }
+        public sbyte LY { get => ly.AxisConvertion(); set => ly = value.AxisConvertion(); }
+        public sbyte RX { get => rx.AxisConvertion(); set => rx = value.AxisConvertion(); }
+        public sbyte RY { get => ry.AxisConvertion(); set => ry = value.AxisConvertion(); }
+        public byte LTrigger { get => lTrigger; set => lTrigger = value; }
+        public byte RTrigger { get => rTrigger; set => rTrigger = value; }
+
+        public byte DPad { get => (byte)buttons[0x000F, 0]; set => buttons[0x000F, 0] = value; }
+        public bool Square { get => buttons[0x0010]; set => buttons[0x0010] = value; }
+        public bool Cross { get => buttons[0x0020]; set => buttons[0x0020] = value; }
+        public bool Circle { get => buttons[0x0040]; set => buttons[0x0040] = value; }
+        public bool Triangle { get => buttons[0x0080]; set => buttons[0x0080] = value; }
+        public bool L1 { get => buttons[0x0100]; set => buttons[0x0100] = value; }
+        public bool R1 { get => buttons[0x0200]; set => buttons[0x0200] = value; }
+        public bool L2 { get => buttons[0x0400]; set => buttons[0x0400] = value; }
+        public bool R2 { get => buttons[0x0800]; set => buttons[0x0800] = value; }
+        public bool Share { get => buttons[0x1000]; set => buttons[0x1000] = value; }
+        public bool Options { get => buttons[0x2000]; set => buttons[0x2000] = value; }
+        public bool L3 { get => buttons[0x4000]; set => buttons[0x4000] = value; }
+        public bool R3 { get => buttons[0x8000]; set => buttons[0x8000] = value; }
+        public bool PS { get => misc[0x01]; set => misc[0x01] = value; }
+        public bool TPad { get => misc[0x02]; set => misc[0x02] = value; }
+        public byte Counter { get => misc[0x3F, 2]; set => misc[0x3F, 2] = value; }
+        public BasicInState() { }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 23)]
+    public struct ExtendedInState
+    {
+        private ushort timestamp = 0;
+        private byte temperature = 0;
+        private short angularVelocityX = 0;
+        private short angularVelocityY = 0;
+        private short angularVelocityZ = 0;
+        private short accelerometerX = 0;
+        private short accelerometerY = 0;
+        private short accelerometerZ = 0;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        private byte[] extData = new byte[5];
+        private BitVector<byte> misc = new();
+        private BitVector<ushort> unk = new();
+        private byte touchCount = 0;
+
+        public ushort Timestamp { get => timestamp; set => timestamp = value; }
+        public byte Temperature { get => temperature; set => temperature = value; }
+        public short AngularVelocityX { get => angularVelocityX; set => angularVelocityX = value; }
+        public short AngularVelocityY { get => angularVelocityY; set => angularVelocityY = value; }
+        public short AngularVelocityZ { get => angularVelocityZ; set => angularVelocityZ = value; }
+        public short AccelerometerX { get => accelerometerX; set => accelerometerX = value; }
+        public short AccelerometerY { get => accelerometerY; set => accelerometerY = value; }
+        public short AccelerometerZ { get => accelerometerZ; set => accelerometerZ = value; }
+        public byte[] ExtData { get => extData; set => extData = value; }
+        public byte Battery { get => misc[0x0F, 0]; set => misc[0x0F, 0] = value; }
+        public bool USB { get => misc[0x10]; set => misc[0x10] = value; }
+        public bool Headphone { get => misc[0x20]; set => misc[0x20] = value; }
+        public bool Mic { get => misc[0x40]; set => misc[0x40] = value; }
+        public bool Ext { get => misc[0x80]; set => misc[0x80] = value; }
+        public bool UnkExt1 { get => unk[0x0001]; set => unk[0x0001] = value; }
+        public bool UnkExt2 { get => unk[0x0002]; set => unk[0x0002] = value; }
+        public bool NotConnected { get => unk[0x0004]; set => unk[0x0004] = value; }
+        public ushort Unk { get => unk[0x1FFF, 3]; set => unk[0x1FFF, 3] = value; }
+        public byte TouchPackCount { get => touchCount; set => touchCount = value; }
+
+        public ExtendedInState() { }
+
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct FingerStatus
+    {
+        private BitVector<uint> fingerData = new();
+
+        private static readonly BitVector<uint>.Section fingerId = BitVector<uint>.CreateSection(0x7F);
+        private static readonly BitVector<uint>.Section fingerX = BitVector<uint>.CreateSection(0xFFF, 8);
+        private static readonly BitVector<uint>.Section fingerY = BitVector<uint>.CreateSection(0xFFF, fingerX);
+
+        public byte FingerId { get => (byte)fingerData[fingerId]; set => fingerData[fingerId] = value; }
+        public bool FingerTouch { get => !fingerData[0x80]; set => fingerData[0x80] = !value; }
+        public short FingerX { get => (short)fingerData[fingerX]; set => fingerData[fingerX] = (uint)value; }
+        public short FingerY { get => (short)fingerData[fingerY]; set => fingerData[fingerY] = (uint)value; }
+        public FingerStatus() { }
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TouchStatus
+    {
+        private byte timestamp = 0;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        private FingerStatus[] fingers = new FingerStatus[2];
+
+        public byte Timestamp { get => timestamp; set => timestamp = value; }
+        public FingerStatus[] Fingers { get => fingers; set => fingers = value; }
+        public TouchStatus() { }
+    }
+
+    #endregion Input
+
+    #region Output
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 32)]
+    public struct USBOutReport : IDS4OutReport
+    {
+        private byte reportId = 0x05;
+        SetStateData state = new();
+
+        public byte ReportId { get => reportId; set => reportId = value; }
+        public SetStateData State { get => state; set => state = value; }
+        public byte[] CRC { get => []; set => _ = value; }
+
+        public USBOutReport() { }
+        public byte[] ToArray()
+        {
+            byte[] raw = new byte[32];
+            GCHandle ptr = GCHandle.Alloc(raw, GCHandleType.Pinned);
+            Marshal.StructureToPtr(this,ptr.AddrOfPinnedObject(), false);
+            //Marshal.Copy(ptr.AddrOfPinnedObject(), raw, 0, raw.Length);
+            ptr.Free();
+            return raw;
+        }
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 78)]//78
+    public struct BTOutReport : IDS4OutReport
+    {
+        private byte reportId = 0x11;
+        private BitVector<byte> misc1 = new(0);
+        private BitVector<byte> misc2 = new(0);
+        private SetStateData state = new();
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 40)]
+        private byte[] padding = new byte[40];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        private byte[] crc = new byte[4];
+
+        public byte ReportId { get => reportId; set => reportId = value; }
+        public byte PollingRate { get => misc1[0x3F, 0]; set => misc1[0x3F, 0] = value; }
+        public bool EnableCRC { get => misc1[0x40]; set => misc1[0x40] = value; }
+        public bool EnableHID { get => misc1[0x80]; set => misc1[0x80] = value; }
+        public byte EnableMic { get => misc2[0x07, 0]; set => misc2[0x07, 0] = value; }
+        public bool UnkA4 { get => misc2[0x08]; set => misc2[0x08] = value; }
+        public bool UnkB1 { get => misc2[0x10]; set => misc2[0x10] = value; }
+        public bool UnkB2 { get => misc2[0x20]; set => misc2[0x20] = value; }
+        public bool UnkB3 { get => misc2[0x40]; set => misc2[0x40] = value; }
+        public bool EnableAudio { get => misc2[0x80]; set => misc2[0x80] = value; }
+        public SetStateData State { get => state; set => state = value; }
+        public byte[] CRC { get => crc; set => crc = value; }
+
+        public BTOutReport() {
+            for (int i = 0; i < padding.Length; i++)
+                padding[i] = 0;
+        }
+        public byte[] ToArray()
+        {
+            byte[] raw = new byte[78];
+            GCHandle ptr = GCHandle.Alloc(raw, GCHandleType.Pinned);
+            Marshal.StructureToPtr(this, ptr.AddrOfPinnedObject(), false);
+            //Marshal.Copy(ptr.AddrOfPinnedObject(), raw, 0, raw.Length);
+            ptr.Free();
+            return raw;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 31)]
+    public struct SetStateData
+    {
+        private BitVector<byte> options = new(0);
+        private BitVector<byte> unk1 = new(0);
+        private byte empty1 = 0;
+        private byte rumbleRight = 0;
+        private byte rumbleLeft = 0;
+        private byte red = 0;
+        private byte green = 0;
+        private byte blue = 0;
+        private byte flashOn = 0;
+        private byte flashOff = 0;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        private byte[] extData = new byte[8];
+        private byte volumeLeft = 0;
+        private byte volumeRight = 0;
+        private byte volumeMic = 0;
+        private byte volumeSpeaker = 0;
+        private BitVector<byte> unkAudio = new(0);
+
+        public bool EnableRumbleUpdate { get => options[0x01]; set => options[0x01] = value; }
+        public bool EnableLedUpdate { get => options[0x02]; set => options[0x02] = value; }
+        public bool EnableLedBlink { get => options[0x04]; set => options[0x04] = value; }
+        public bool EnableExtWrite { get => options[0x08]; set => options[0x08] = value; }
+        public bool EnableVolumeLeftUpdate { get => options[0x10]; set => options[0x10] = value; }
+        public bool EnableVolumeRightUpdate { get => options[0x20]; set => options[0x20] = value; }
+        public bool EnableVolumeMicUpdate { get => options[0x40]; set => options[0x40] = value; }
+        public bool EnableVolumeSpeakerUpdate { get => options[0x80]; set => options[0x80] = value; }
+
+        public bool UnkReset1 { get => unk1[0x01]; set => unk1[0x01] = value; }
+        public bool UnkReset2 { get => unk1[0x02]; set => unk1[0x02] = value; }
+        public bool Unk1 { get => unk1[0x04]; set => unk1[0x04] = value; }
+        public bool Unk2 { get => unk1[0x08]; set => unk1[0x08] = value; }
+        public bool Unk3 { get => unk1[0x10]; set => unk1[0x10] = value; }
+        public byte UnkPad1 { get => unk1[0x07, 5]; set => unk1[0x07, 5] = value; }
+        
+        public byte Empty1 { get => empty1; set => empty1 = value; }
+
+        public byte RumbleRight { get => rumbleRight; set => rumbleRight = value; }
+        public byte RumbleLeft { get => rumbleLeft; set => rumbleLeft = value; }
+        public byte Red { get=> red; set => red = value; }
+        public byte Green { get=> green; set => green = value; }
+        public byte Blue { get => blue; set => blue = value; }
+        public byte FlashOn { get=> flashOn; set => flashOn = value; }
+        public byte FlashOff { get=> flashOff; set => flashOff = value; }
+
+        public byte[] ExtData { get=> extData; set => extData = value; }
+        public byte VolumeLeft { get=> volumeLeft; set => volumeLeft = value; }
+        public byte VolumeRight { get=> volumeRight; set => volumeRight = value; }
+        public byte VolumeMic { get=> volumeMic; set => volumeMic = value; }
+        public byte VolumeSpeaker { get=> volumeSpeaker; set => volumeSpeaker = value; }
+
+        public byte UnkAudio1 { get => unkAudio[0x7F, 0]; set => unkAudio[0x7F, 0] = value; }
+        public bool UnkAudio2 { get => unkAudio[0x80]; set => unkAudio[0x80] = value; }
+
+        public SetStateData()
+        {
+            for (int i = 0; i < extData.Length; i++)
+                extData[i] = 0;
+        }
+    }
+    #endregion Output
+}
